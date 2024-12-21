b25520e32334ec4b9b1ec5fb411bc8a7be7d6ac4
AceMouse <humle000@gmail.com>
2024-12-21 14:47:21 +0100

    include sim_sort in ays file

--- are_you_sure.cpp
+++ are_you_sure.cpp
@@ -3,7 +3,14 @@
 #include <string>
 #include <vector>
 #include <limits>
-#include "similarity_sort.cpp"
+#include <iostream>
+
+#include "includes/levenshtein-sse.hpp"
+
+#include <lemon/smart_graph.h>
+#include <lemon/network_simplex.h>
+#include <lemon/cost_scaling.h>
+#include <lemon/graph_to_eps.h>
 
 typedef uint32_t AYS_fixture_id;
 typedef uint32_t AYS_provider_id;
@@ -16,12 +23,11 @@ typedef bool (*AYS_fixture_comparison_func)(const struct AYS_fixture&, const str
 
 struct AYS_fixture;
 struct AYS_event;
-std::string AYS_event_to_string(const AYS_event event); 
-std::string AYS_fixture_to_string(const AYS_fixture fixture);
-bool AYS_event_arb(AYS_event &event);
 std::string AYS_event_to_string(const AYS_event event);
 std::string AYS_fixture_to_string(const AYS_fixture fixture);
 std::vector<AYS_event> AYS_fixtures_to_events(std::vector<AYS_fixture> fs);
+
+
 
 struct AYS_fixture {
     uint64_t unix_time;
@@ -168,6 +174,118 @@ bool AYS_sid_compare(const AYS_fixture &f1, const AYS_fixture &f2){
     return f1.sid < f2.sid;
 }
 
+int min_weight_matching(const int pairs, const std::vector<std::vector<int>> &weightMatrix, std::vector<int> &solution){
+    using Graph = lemon::SmartDigraph;
+    using Node = Graph::Node;
+    using Arc = Graph::Arc;
+    using MCF = lemon::NetworkSimplex<Graph, int32_t, int32_t>;
+
+    int nodeNum = pairs*2+2; // worker and task in each pair + source and sink
+    int arcNum = pairs*(pairs+2); // all to all between workers and tasks 
+                                  // + one to all source to workers
+                                  // + all to one tasks to sink 
+    
+    Graph g;
+    std::vector<Node> nodes(nodeNum);
+    std::vector<Arc> arcs(arcNum);
+    Graph::ArcMap<int32_t> weights(g);
+    Graph::ArcMap<int32_t> capacities(g);
+    for(int i = 0; i < nodeNum; i++){
+        nodes[i] = g.addNode();
+    }
+    int sourceIDX = 0;
+    int sinkIDX = nodeNum-1;
+    Node sourceNode = nodes[sourceIDX];
+    Node sinkNode = nodes[sinkIDX];
+    Arc arc;
+    Node toNode;
+    Node fromNode;
+    int arcIDX = 0; 
+    for (int to = 1; to < nodeNum-1; to+=2){ // source to workers
+        toNode = nodes[to];
+        arc = g.addArc(sourceNode, toNode);
+        capacities[arc] = 1;
+        weights[arc] = 1;
+        //std::cout << "edge: " << sourceIDX << "->" << to << " @ " << weights[arc] << std::endl ;
+        arcs[arcIDX++] = arc;
+    }
+    for (int from = 2; from < nodeNum-1; from+=2){ // tasks to sink
+        fromNode = nodes[from];
+        arc = g.addArc(fromNode, sinkNode);
+        capacities[arc] = 1;
+        weights[arc] = 1;
+        //std::cout << "edge: " << from << "->" << sinkIDX << " @ " << weights[arc] << std::endl ;
+        arcs[arcIDX++] = arc;
+    }
+    for (int worker = 0; worker < pairs; worker++ ){ // workers to tasks
+        for (int task = 0; task < pairs; task++ ){
+            int from = worker*2+1;
+            int to = task*2+2;
+            toNode = nodes[to];
+            fromNode = nodes[from];
+            arc = g.addArc(fromNode, toNode);
+            capacities[arc] = 1;
+            weights[arc] = weightMatrix[worker][task];
+            //std::cout << "edge: " << from << "->" << to << " @ " << weights[arc] << std::endl ;
+            arcs[arcIDX++] = arc;
+        }
+    }
+
+    Graph::ArcMap<int32_t> flows(g);
+    MCF solver(g);
+    MCF::ProblemType status = solver.upperMap(capacities).costMap(weights).stSupply(sourceNode, sinkNode, pairs).run();
+    switch (status) 
+    {
+        case MCF::INFEASIBLE:
+            std::cerr << "insufficient flow" << std::endl;
+            return 1;
+        case MCF::OPTIMAL:
+            solver.flowMap(flows);
+            for (int worker = 0; worker < pairs; worker++ ){ // workers to tasks
+                for (int task = 0; task < pairs; task++ ){
+                    arc = arcs[pairs*2 + worker*pairs + task];
+                    if(flows[arc] != 0){
+                        solution[worker] = task;
+                    }
+                }
+            }
+            break;
+        case MCF::UNBOUNDED:
+            std::cerr << "infinite flow" << std::endl;
+            return 2;
+        default:
+            break;
+    }
+    return 0;
+}
+
+
+float similarity_sort(const std::vector<std::string> &a1,const std::vector<std::string> &a2, std::vector<int> *solution){
+    if (a1.size() != a2.size()){
+        std::cerr << "different lengths a1:" <<a1.size() << " != a2:" << a2.size() << std::endl;
+        return 1.f;
+    }
+    int pairs = a1.size();
+    (*solution).resize(pairs);
+    std::vector<std::vector<int>> sim_matrix(pairs, std::vector<int> (pairs, 0)) ;
+    for (int i = 0; i<pairs; i++){
+        for (int j = 0; j<pairs; j++){
+            sim_matrix[i][j] = levenshteinSSE::levenshtein(a1[i], a2[j]);//https://stackoverflow.com/a/24063783   to lower?
+        }
+    }
+    if (min_weight_matching(pairs, sim_matrix, *solution)){
+        return 1.f;
+    }
+    float max = 0;
+    for (int i = 0; i<pairs; i++){
+        float x = (float)sim_matrix[i][(*solution)[i]]/((a1[i].size() + a2[i].size()) >>1);
+        if (x>max){
+            max = x;
+        }
+    }
+
+    return max; 
+}
 
 void split_on_diff(std::vector<AYS_fixture> fs, 
                    std::vector<std::vector<AYS_fixture>> &out,
--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 set -ex
-#g++ are_you_sure.cpp -Wall -g -o are_you_sure
-g++ -I ~/lemon/include example.cpp -g -O3 -DNDEBUG -march=native -std=c++11 -Wall -Wno-maybe-uninitialized -L ~/lemon/lib -lemon -o example
-#g++ -I ~/lemon/include similarity_sort.cpp -g -O3 -DNDEBUG -march=native -std=c++11 -Wall -Wno-maybe-uninitialized -L ~/lemon/lib -lemon -o similarity_sort
+g++ -I ~/lemon/include example.cpp -O3 -DNDEBUG -march=native -std=c++11 -Wall -Wno-maybe-uninitialized -L ~/lemon/lib -lemon -o example
